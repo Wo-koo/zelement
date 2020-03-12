8db6a676b9570a26cde961444ffa8e7922704646
Wo-koo <981652081@qq.com>
2020-03-12 23:47:28 +0800

    modify new.js to auto create some component's template:

--- build/bin/new.js
+++ build/bin/new.js
@@ -12,6 +12,56 @@ if (!process.argv[2]) {
 const path = require('path');
 const fs =  require('fs');
 const fileSave = require('file-save');
-const componentName = process.argv[2]; // detail reference node.js
-const componentName;
+const uppercamelcase = require('uppercamelcase');
+const componentName = process.argv[2]; //process.argv detail reference node.js
+const componentChineseName = process.argv[3] || componentName;
+const ComponentName = uppercamelcase(componentName);
+const componentPath = path.resolve(__dirname,'../../src/components',componentName);
+const Files = [
+    {
+        // component's index file template
+        filename: 'index.js',
+        content:`import ${ComponentName} from './src/${ComponentName}';
+
+        ${ComponentName}.install = function(Vue){
+            Vue.component(${ComponentName}.name, ${ComponentName});
+        }
+
+        export default ${ComponentName};
+        `
+    },
+    // component's vue file template
+    {
+        filename:`src/${ComponentName}.vue`,
+        content:`<template>
+        <div class='z-${componentName}'></div>
+        </template>
+        
+        <script>
+        
+        export default{
+            name:"Z${ComponentName}",
+        };
+        </script>`
+    }
+];
+
+// add new component to components.json
+const componentFile = require('../../components.json');
+if (componentFile[componentname]) {
+    console.error(`${componentname} component already exit`);
+    process.exit(`${componentname} already exit`);
+}
+componentFile[componentname] = `./src/components/${componentname}/index.js`;
+fileSave(path.join(__dirname,'../../components.json'))
+    .write(JSON.stringify(componentFile,null," "),'utf8')
+    .end('\n');
+
+// to create new component
+Files.forEach(item=>{
+    fileSave(path.join(componentPath,item.filename))
+        .write(item.content,'utf8')
+        .end('\n');
+});
+
 
