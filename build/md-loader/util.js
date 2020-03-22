47160f1f7a885b588a12da4450ef324b9d005142
Wo-koo <981652081@qq.com>
2020-03-23 01:20:49 +0800

    modify some ulti javaScript codes

--- build/md-loader/util.js
+++ build/md-loader/util.js
@@ -5,12 +5,14 @@ const compiler = require('vue-template-compiler');
 
 // 剥离脚本部分
 function stripScript(content){
-
+    let script = content.match(/<(script)>([\s\S]+)<(\/script>)/);
+    return script && script[2] ? script[2].trim() : ''; // 如果可以捕获script标签中的内容，则进行返回。否则返回为空
 }
 
 // 剥离样式部分
 function stripStyle(content){
-
+    let style = content.match(/<([css,scss])([\s\S])+<\/\1>/);
+    return style && style[2] ?? style[2] : '';
 }
 
 // 剥离模版部分
