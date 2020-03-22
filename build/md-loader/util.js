5b11d78c761986cf15365ebbc1b3c59103ea306b
Wo-koo <981652081@qq.com>
2020-03-23 01:02:34 +0800

    add some code to strip codes from md

---
+++ build/md-loader/util.js
@@ -0,0 +1,35 @@
+// these are uitls for md-loader
+
+const {compileTemplate} = require('@vue/component-compiler-utils'); // ?
+const compiler = require('vue-template-compiler');
+
+// 剥离脚本部分
+function stripScript(content){
+
+}
+
+// 剥离样式部分
+function stripStyle(content){
+
+}
+
+// 剥离模版部分
+function stripTemplate(content){
+
+}
+
+// ?
+function pad(source){
+
+}
+
+function generatorInLineComponentText(template,script){
+
+}
+
+module.export = {
+    stripScript,
+    stripStyle,
+    stripTemplate,
+    generatorInLineComponentText,
+}
