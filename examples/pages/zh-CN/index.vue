4c356a748518869e37f7b82825c187f500247eee
Wo-koo <981652081@qq.com>
2020-03-25 17:37:21 +0800

    add template file with zh-CN

---
+++ examples/pages/zh-CN/index.vue
@@ -0,0 +1,17 @@
+<!--这个地方有个处理技巧，就是先按照html的格式进行编码，然后再修改成后缀为tpl的文件-->
+<style>
+
+</style>
+
+<template>
+    <div class="banner">
+        <div class="banner_desc">
+            <h1>高性能组件</h1>
+            <p>基于vue2.0的高性能组件</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
