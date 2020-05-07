d00a08d6135a7b4432e492e06adc9b053c4bb452
Wo-koo <981652081@qq.com>
2020-05-08 00:57:41 +0800

    modify

--- examples/play.js
+++ examples/play.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './play/index.vue';
 import ZElement from 'main/index.js';
 import ElementUI from 'element-ui';
+import "../src/theme/src/alert.scss";
 
 Vue.use(ZElement);
 Vue.use(ElementUI);
