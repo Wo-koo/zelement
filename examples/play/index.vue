90265150d3a2f1b703d544cba21de1ee6f2b66d8
Wo-koo <981652081@qq.com>
2020-05-21 00:39:55 +0800

    add button 3d default codes

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -5,6 +5,7 @@
         <z-alert type='error' title='ZAlertTest' description='This is an message from ZAlert'/>
         <z-alert type='warning' title='ZAlertTest' description='This is an message from ZAlert'/>
         <z-button>CommonButton</z-button>
+        <button> window Button</button>
     </div>
 </template>
 
