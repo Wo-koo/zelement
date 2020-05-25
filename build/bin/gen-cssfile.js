b740770ab93c660931743bace2c6accaa1a6c871
Wo-koo <981652081@qq.com>
2020-05-25 23:09:51 +0800

    add scss file auto create codes

--- build/bin/gen-cssfile.js
+++ build/bin/gen-cssfile.js
@@ -2,3 +2,37 @@
 用来生成css文件的
 **/
 
+const fs = require('fs');
+const path = require('path');
+var Components = require('../../components.json');
+const themes = ['theme'];
+
+Components = Object.keys(Components);
+const basePath = path.resolve(__dirname,'../../src');
+
+function fileExists(filePath){
+    try{
+        return fs.statSync(filePath).isFile();
+    }catch(e){
+        console(`${{filePath}} not exist files`);
+        return false;
+    }
+}
+
+themes.forEach(theme=>{
+    let isScss = theme == 'theme' ? true :false;
+    var indexContent = '';
+
+    Components.forEach(key=>{
+        var fileName = key + (isScss ? '.scss' : 'css');
+        indexContent += '@import "./' + fileName + '"; \n';
+        let filePath = path.resolve(basePath,'theme/src',fileName);
+        if (!fileExists(filePath)) {
+            console.warn(`scss file ${{fileName}} miss out!`);
+            fs.writeFileSync(filePath,'','utf-8');
+        }
+    });
+    fs.writeFileSync(path.resolve(basePath,'theme/src',isScss ? 'index.scss' :'index.css'),indexContent);
+});
+
+
