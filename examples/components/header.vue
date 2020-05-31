d6646c9031d43e0c3777b0747938d20f7d640f7e
Wo-koo <981652081@qq.com>
2020-05-31 22:34:52 +0800

    add components/header.vue's code

---
+++ examples/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="headerWrapper">
+        <header class="header">
+            <div class="container">
+                <slot>
+                    ZElement 
+                </slot>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <router-link active-class="active" 
+                        :to="`/${ lang }/guide `">
+                            {{langConfig.guide}}
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link acctive-class="active" :to="`/${lang}/component`">
+                        {{langConfig.components}}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+
+    },
+    computed:{
+        lang(){
+            return this.$route.path.split('/')[1] || 'zh-CN';
+        }
+    }
+}
+</script>
