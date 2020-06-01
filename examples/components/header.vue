565e0ad75e60e41b8cd255fb08239c74ecba217e
Wo-koo <981652081@qq.com>
2020-06-01 22:32:51 +0800

    add header.vue style scss codes

--- examples/components/header.vue
+++ examples/components/header.vue
@@ -1,10 +1,56 @@
+<style lang="scss" scope>
+    .headerWrapper{
+        height: 80px;
+    }
+    .header{
+        height: 80px;
+        background-color: #FFF;
+        width: 100%;
+        position: relative;
+        .container{
+            box-sizing: border-box;
+            border-bottom: 1px solid #DCDFE6;
+            .logo{
+                font-size: 3em;
+                font-weight: bold;
+                color:goldenrod;
+            }
+        }
+        .nav{
+            float: right;
+            height: 100%;
+            line-height: 80px; // 这个line-height是什么属性？
+            background-color: greenyellow;
+            &::before, &::after{
+                display: table;
+                content: "";
+            }
+            &::after{
+                clear: both;
+            }
+        }
+        .nav-item{
+            float: left;
+            list-style: none ;
+            position: relative;
+            margin: 0;
+            a{
+                display: block;
+                text-decoration: none;
+                color: #1989FA;
+                padding: 0 22px;
+            }
+        }
+    }
+
+</style>
 <template>
     <div class="headerWrapper">
         <header class="header">
             <div class="container">
-                <slot>
+                <span class="logo">
                     ZElement 
-                </slot>
+                </span>
                 <ul class="nav">
                     <li class="nav-item">
                         <router-link active-class="active" 
